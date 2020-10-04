d67968c9dc8a1ff2d157dba654a4f83592c93c48
rekunova <ma.rekunova@gmail.com>
2020-10-04 22:29:16 +0300

    links generator

--- scripts/index.js
+++ scripts/index.js
@@ -18,3 +18,71 @@ $('html').mousemove(function (e) {
     }
 });
 
+function handleProjectClick(){
+   const campaignLinks = document.querySelectorAll('.tile-link');
+
+   campaignLinks.forEach((link)=> {
+       link.addEventListener('click', (e) => {
+           const project = e.target.closest('.tile-link')
+           const name = project.dataset.name; 
+           showCampaignPage(name);
+       })
+   })
+}
+
+handleProjectClick();
+
+function showCampaignPage(name){
+    const campaignPage = document.querySelector('.body-campaign')
+    const campaign = campaigns.find(x => x.name === name);
+
+    renderCampaign(campaign);
+    campaignPage.style.display='block';
+}
+
+
+function renderCampaign(campaign) {
+    const name = document.querySelector('.name-value');
+    const format = document.querySelector('.format-value');
+    const services = document.querySelector('.services-value');
+    const description = document.querySelector('.campaign-description');
+    const url = document.querySelector('.campaign-button-link');
+    const videoUrl = document.querySelector('.campaign-video-src');
+
+    name.innerText = campaign.name;
+    format.innerText = campaign.format;
+    services.innerText = campaign.services;
+    description.innerText = campaign.description;
+    url.setAttribute('href', campaign.url);
+    videoUrl.setAttribute('href', campaign.videoUrl);
+}
+
+const campaigns = [
+    {
+        name: "wyevale",
+        format: "Desktop banner advertisement",
+        services: "Design, build, animation",
+        description: "Wyevale have made beautiful use of colour in their spring campaign with us. Plants grow across the screen, the campaign changes colour on scroll which gives control to the user, and the bees in the header wobble subtly to attract attention.",
+        url: "https://qna.habr.com/q/498074",
+        videoUrl: "../assets/video/wyevale.mp4"
+
+    },
+    {
+        name: "Dune",
+        format: "Desktop banner advertisement",
+        services: "Design, build, animation",
+        description: "Wyevale have made beautiful use of colour in their spring campaign with us. Plants grow across the screen, the campaign changes colour on scroll which gives control to the user, and the bees in the header wobble subtly to attract attention.",
+        url: "https://qna.habr.com/q/498074",
+        videoUrl: "../assets/video/wyevale.mp4"
+
+    },
+    {
+        name: "Adidas",
+        format: "Desktop banner advertisement",
+        services: "Design, build, animation",
+        description: "Wyevale have made beautiful use of colour in their spring campaign with us. Plants grow across the screen, the campaign changes colour on scroll which gives control to the user, and the bees in the header wobble subtly to attract attention.",
+        url: "https://qna.habr.com/q/498074",
+        videoUrl: "../assets/video/wyevale.mp4"
+
+    }
+]
