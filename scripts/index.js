2261cdf03a618ffb4515e85e888b72355acd6e74
rekunova <ma.rekunova@gmail.com>
2020-10-23 21:47:43 +0300

    eyes

--- scripts/index.js
+++ scripts/index.js
@@ -3,36 +3,107 @@ const timeStart = Date.now();
 window.onload = () => {
     const loadingTime = Date.now() - timeStart
     const loader = document.querySelector('.loader');
-    if(loadingTime > 2000){     // if loaded faster then 2s
+    if (loadingTime > 2500) {     // if loaded faster then 2s
         loader.style.display = "none";
         bodyMain.classList.remove('lock');
     }
-    else{
+    else {
         setTimeout(() => {
             loader.style.display = "none";
             bodyMain.classList.remove('lock');
-        }, 2000);               
+        }, 2500);
     }
-    
+
 }
 
 var el1 = $('#white'), eyeBall1 = el1.find('div');
 el1.show();
 var x1 = el1.offset().left + 37, y1 = el1.offset().top + 25;
 var r = 10, x, y, x2, y2, isEyeProcessed = false;
-$('html').mousemove(function (e) {
-    if (!isEyeProcessed) {
-        isEyeProcessed = true;
-        var x2 = e.pageX, y2 = e.pageY;
-        y = ((r * (y2 - y1)) / Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))) + y1;
-        x = (((y - y1) * (x2 - x1)) / (y2 - y1)) + x1;
-        eyeBall1.css({
-            marginTop: (y - y1 + 1) + 'px',
-            marginLeft: (x - x1) + 'px'
-        });
-        isEyeProcessed = false;
-    }
-});
+
+
+function isTouchDevice() {
+    return true == ("ontouchstart" in window || window.DocumentTouch && document instanceof DocumentTouch);
+}
+
+if (isTouchDevice() === true) {
+    // alert('Touch Device'); //your logic for touch device
+
+
+    var initialPoint;
+    var finalPoint;
+    document.addEventListener('touchstart', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        initialPoint = event.changedTouches[0];
+    }, false);
+    document.addEventListener('touchend', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        finalPoint = event.changedTouches[0];
+        var xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
+        var yAbs = Math.abs(initialPoint.pageY - finalPoint.pageY);
+        if (xAbs > 20 || yAbs > 20) {
+            //             if (xAbs > yAbs) {
+            //                 if (finalPoint.pageX < initialPoint.pageX) {
+            //                     /*СВАЙП ВЛЕВО*/
+            // }
+            //                 else {
+            //                     /*СВАЙП ВПРАВО*/
+            // }
+            //             }
+            //  {
+            if (finalPoint.pageY < initialPoint.pageY) {
+                /*СВАЙП ВВЕРХ*/
+                eyeBall1.css({
+                    marginTop: 3 + 'px',
+                });
+                console.log(20);
+            }
+            else {
+                eyeBall1.css({
+                    marginTop: -2 + 'px',
+                });
+                console.log(20);
+            }
+            // }
+        }
+    }, false);
+}
+else {
+    // alert('Not a Touch Device'); //your logic for non touch device
+    $('html').mousemove(function (e) {
+        if (!isEyeProcessed) {
+            isEyeProcessed = true;
+            var x2 = e.pageX, y2 = e.pageY;
+            y = ((r * (y2 - y1)) / Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))) + y1;
+            x = (((y - y1) * (x2 - x1)) / (y2 - y1)) + x1;
+            eyeBall1.css({
+                marginTop: (y - y1 + 1) + 'px',
+                marginLeft: (x - x1) + 'px'
+            });
+            isEyeProcessed = false;
+        }
+    });
+}
+
+// $('html').mousemove(function (e) {
+//     if (!isEyeProcessed) {
+//         isEyeProcessed = true;
+//         var x2 = e.pageX, y2 = e.pageY;
+//         y = ((r * (y2 - y1)) / Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))) + y1;
+//         x = (((y - y1) * (x2 - x1)) / (y2 - y1)) + x1;
+//         eyeBall1.css({
+//             marginTop: (y - y1 + 1) + 'px',
+//             marginLeft: (x - x1) + 'px'
+//         });
+//         isEyeProcessed = false;
+//     }
+// });
+
+
+
+
 
 function handleProjectClick() {
     const campaignLinks = document.querySelectorAll('.tile-link');
